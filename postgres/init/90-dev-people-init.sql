361720ce503c77e6cdfcc451f41395c916a72bd4
Chris Rose <offline@offby1.net>
2018-07-20 07:18:38 -0700

    Fix some typos in the development people setup

--- postgres/init/90-dev-people-init.sql
+++ postgres/init/90-dev-people-init.sql
@@ -1,13 +1,13 @@
 INSERT INTO admin.Admins VALUES ('admin@example.com', true, true, true);
 
-SET ROLE membbers;
+SET ROLE members;
 
 INSERT INTO People (legal_name, email, membership, member_number, can_hugo_nominate, can_hugo_vote)
      VALUES ('Admin', 'admin@example.com', 'NonMember', NULL, false, false),
             ('First Member', 'member@example.com', 'FirstWorldcon', 21, true, true),
             ('Fan Parent', 'family@example.com', 'Adult', 37, true, true),
             ('Fan Child', 'family@example.com', 'Child', 45, false, false),
-            ('Fan Youth', 'family@example.com', 'Youth', 59, true, true),
+            ('Fan Youth', 'family@example.com', 'YoungAdult', 59, true, true),
             ('Fan Supporter', 'supporter@example.com', 'Supporter', 68, true, true),
             ('Dupe Supporter', 'supporter@example.com', 'Supporter', 76, false, false),
             ('Fan Trader', 'trader@example.com', 'Exhibitor', 84, false, false),
