a18b56d3b0df5fbbc4d64f5cfff2952f92a9e2c7
Chris Rose <offline@offby1.net>
2018-08-08 21:22:47 -0700

    More robust validation

--- deploy_scripts/validate.sh
+++ deploy_scripts/validate.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-has_memberships=$(curl -s -L http://localhost/api/purchase/prices | jq -r '. | has("memberships")')
+while true; do
+    currently=$(systemctl is-active registration)
+    if [ "$currently" == "active" ]; then
+        break
+    fi
+    echo "Waiting for systemd to think our service is active; is currently $currently"
+    sleep 1
+done
+
+has_memberships=$(curl -k -s -L http://localhost/api/purchase/prices | jq -r '. | has("memberships")')
 if [[ "$has_memberships" == "true" ]]; then
     exit 0
 else
